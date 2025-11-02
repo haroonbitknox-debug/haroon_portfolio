c23e9a7caeb9b5e1303189d2ed59364ca5b690cd
Haroon_Bitknox <haroonbitknox@gmail.com>
2025-11-02 16:44:25 +0500

    added alert box on send message button

--- src/components/pages/ContactForm.jsx
+++ src/components/pages/ContactForm.jsx
@@ -19,7 +19,7 @@ function ContactForm() {
         )
     }
   const handleFormSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         setSuccess("Loading");
   const formUrl  = 
   "https://docs.google.com/forms/d/e/1FAIpQLSf5q1D0iOOU0UP1hhXlEz-SKmLNLXub3eXDSrnpIhwlJ_-2Cg/formResponse";
@@ -39,7 +39,8 @@ function ContactForm() {
             useremail: "",
             usermessage: ""
         })
-  }).catch((error)=>{setError(error.message)})
+  }).catch((error)=>{setError(error.message)});
+  alert("Haroon has received your message!")
     }
     return (
     <>
